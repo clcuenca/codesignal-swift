5c8d8767d0f730ec4906b78e62e2daecec7bac41
carloscuenca <cuenca.cl@gmail.com>
2020-05-19 18:40:52 -0700

    Initial Commit

---
+++ CodeSignal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodeSignal
+//
+//  Created by Carlos Cuenca on 5/19/20.
+//  Copyright © 2020 Carlos Cuenca. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
