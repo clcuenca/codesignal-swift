de1b9a91cdaa883e145c55b1faef042043aed28c
Carlos Cuenca <cuenca.cl@gmail.com>
2020-07-05 15:04:49 -0700

    add countSumOfTwoRepresentations2

--- CodeSignal/LoopTunnel.swift
+++ CodeSignal/LoopTunnel.swift
@@ -28,5 +28,31 @@ class LoopTunnel {
         return result
         
     }
+
+    /**
+      Returns the amount of ways to sum the integer n
+    
+      Problem #26
+    
+      - Author: Carlos L. Cuenca
+      - Date: 07/05/2020
+    */
+    internal static func countSumOfTwoRepresentations2(int n, int l, int r) {
+
+        var count = 0
+
+        for index in l...r {
+
+            if(index <= (n - index) && (n - index) <= r) { 
+                
+                count += 1
+                
+            }
+
+        }
+
+        return count
+
+    }
     
 }
