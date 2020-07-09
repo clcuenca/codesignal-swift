b842f195aaa9756417e85b241f04833f4db2b254
Carlos Cuenca <cuenca.cl@gmail.com>
2020-07-08 17:35:26 -0700

    implement largestNumber

--- CodeSignal/IntroGates.swift
+++ CodeSignal/IntroGates.swift
@@ -13,3 +13,20 @@ func addTwoDigits(n: Int) -> Int {
 	return (n % 10) + (n / 10)
 
 }
+
+/**
+  Returns the largest base 10 number given the amount of digits, n
+ 
+  Problem #2
+ 
+  -Author: Carlos L. Cuenca
+  -Date: 07/08/2020
+  -Parameter n: int the amount of digits the number should have
+  -Returns: The largest resulting number with n digits
+ */
+
+func largestNumber(n: Int) -> Int {
+	
+	return Int(pow(10, Double(n))) - 1
+
+}
